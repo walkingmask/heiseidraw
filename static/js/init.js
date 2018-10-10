b655706ea98bbf58499e4d0752047d9c4cd8a7ff
walkingmask <walkingmask@gmail.com>
2018-10-10 19:52:07 +0900

    Revert "Revert "Merge pull request #2 from tyru/fix-canvas-size-on-pc""
    This reverts commit 7d7860f2343e01187cb484facf277fd9ea9c6d68.

--- static/js/init.js
+++ static/js/init.js
@@ -3,15 +3,24 @@
     const width = container.width();
     const height = container.height();
     const base = (width * (55/40) > height)? height : base;
+    const board = $('#board');
 
-    let board = $('#board');
-    if (base === width) {
-        board.width(width);
-        board.height(width * (55/40));
-    }
-    else {
-        board.width(height * (40/55));
-        board.height(height);
+    if (window.matchMedia('(max-width:480px)').matches) {
+        // 画面幅 480px 以下 (スマホ) の場合は可変幅
+        if (base === width) {
+            board.width(width);
+            board.height(width * (55/40));
+        }
+        else {
+            board.width(height * (40/55));
+            board.height(height);
+        }
+    } else {
+        // 画面幅 480px 以上 (タブレット、PC) の場合は固定幅
+        // (画像をドラッグアンドドロップで貼り付けた際にキャンバスが可変だと
+        // 画像の高さ・幅の調整が面倒になるため)
+        board.width(480);
+        board.height(640);
     }
 })();
 
