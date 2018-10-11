229b3cdddce8dc8d38dd42450e2dc77e3199acfc
walkingmask <walkingmask@gmail.com>
2018-10-11 23:11:46 +0900

    Revert "Merge pull request #6 from tyru/add-image-button"
    This reverts commit 4b404ad021f7e95855d77d7e4dfd28e630d74d30, reversing
    changes made to b0fef688bdcda59c1dcce1ebe11fda391a57039e.

--- static/js/init.js
+++ static/js/init.js
@@ -75,5 +75,4 @@ var board = new DrawingBoard.Board('board', {
     $('.drawing-board-control-navigation-back').width(base).height(base).css("fontSize","3vh");
     $('.drawing-board-control-navigation-forward').width(base).height(base).css("fontSize","3vh");
     $('.drawing-board-control-navigation-reset').width(base).height(base).css("fontSize","3vh");
-    $('#image-chooser').width(base).height(base);
 })();
